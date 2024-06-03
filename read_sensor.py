a34e39253abb6a39652ff962cf827735403343de
anh210802 <77352818+anh210802@users.noreply.github.com>
2024-06-03 08:37:37 +0700

    Update read_sensor.py

--- read_sensor.py
+++ read_sensor.py
@@ -1,61 +1,84 @@
-print("Test for read sensor by RS485")
+print("Sensors and Actuators")
 
-import serial
 import time
+import serial.tools.list_ports
 
+def getPort():
+    ports = serial.tools.list_ports.comports()
+    N = len(ports)
+    commPort = "None"
+    for i in range(0, N):
+        port = ports[i]
+        strPort = str(port)
+        if "USB" in strPort:
+            splitPort = strPort.split(" ")
+            commPort = (splitPort[0])
+    return commPort
+    # return "/dev/ttyUSB1"
 
 portName = "/dev/ttyUSB0"
+print(portName)
+
+
 
 try:
     ser = serial.Serial(port=portName, baudrate=115200)
-    print("Open port successfully")
+    print("Open successfully")
 except:
-    print("Can not open port")
+    print("Can not open the port")
+
+relay1_ON  = [0, 6, 0, 0, 0, 255, 200, 91]
+relay1_OFF = [0, 6, 0, 0, 0, 0, 136, 27]
+
+def setDevice1(state):
+    if state == True:
+        ser.write(relay1_ON)
+    else:
+        ser.write(relay1_OFF)
+    time.sleep(1)
+    print(serial_read_data(ser))
+
 
 def serial_read_data(ser):
     bytesToRead = ser.inWaiting()
     if bytesToRead > 0:
         out = ser.read(bytesToRead)
-        return out
-    else:
-        return 0
-    
-while True:
-    print(serial_read_data(ser))
+        data_array = [b for b in out]
+        print(data_array)
+        if len(data_array) >= 7:
+            array_size = len(data_array)
+            value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
+            return value
+        else:
+            return -1
+    return 0
+
+soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
+def readTemperature():
+    serial_read_data(ser)
+    ser.write(soil_temperature)
     time.sleep(1)
+    return serial_read_data(ser)
 
-# def serial_read_data(ser):
-#     bytesToRead = ser.inWaiting()
-#     if bytesToRead > 0:
-#         out = ser.read(bytesToRead)
-#         data_array = [b for b in out]
-#         print(data_array)
-#         if len(data_array) >= 7:
-#             array_size = len(data_array)
-#             value = data_array[array_size - 4] * 256 + data_array[array_size - 3]
-#             return value
-#         else:
-#             return -1
-#     return 0
+soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
+def readMoisture():
+    serial_read_data(ser)
+    ser.write(soil_moisture)
+    time.sleep(1)
+    return serial_read_data(ser)
 
-# soil_temperature =[1, 3, 0, 6, 0, 1, 100, 11]
-# def readTemperature():
-#     serial_read_data(ser)
-#     ser.write(soil_temperature)
-#     time.sleep(1)
-#     return serial_read_data(ser)
-
-# soil_moisture = [1, 3, 0, 7, 0, 1, 53, 203]
-# def readMoisture():
-#     serial_read_data(ser)
-#     ser.write(soil_moisture)
-#     time.sleep(1)
-#     return serial_read_data(ser)
-
-# while True:
-#     print("TEST SENSOR")
-#     print(readMoisture())
-#     time.sleep(1)
-#     print(readTemperature())
-#     time.sleep(1)
-
+counter = 0
+while True:
+    if counter >= 10:
+        setDevice1(True)
+        print("Relay1 : ON")
+    else:
+        setDevice1(False)
+        print("Relay2 : OFF")
+    if counter == 20:
+        counter = 0
+    counter += 1
+    print(readMoisture())
+    time.sleep(0.5)
+    print(readTemperature())
+    time.sleep(0.5)
